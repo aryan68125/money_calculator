bf38993101621203f98959e5366a72fb0131a1f7
aryan68125 <aryan68125@gmail.com>
2021-10-22 01:49:07 +0530

    added expenses and income summary

--- static/js/expenseChartSelect.js
+++ static/js/expenseChartSelect.js
@@ -1,34 +1,43 @@
 //get access to the expense summary chart for past-one-month
+const myChart_container = document.getElementById('myChart_container');
 const myChart_title = document.getElementById('myChart_title');
 const myChart = document.getElementById('myChart');
 
 //get access to the expense summary chart for past-six-month
+const myChart2_container = document.getElementById('myChart2_container');
 const myChart2_title = document.getElementById('myChart2_title');
 const myChart2 = document.getElementById('myChart2');
 
 //get access to the expense summary chart for past-twelve-month
+const myChart3_container = document.getElementById('myChart3_container');
 const myChart3_title = document.getElementById('myChart3_title');
 const myChart3 = document.getElementById('myChart3');
 
 //hide past 1 month, past 6 month and past 12 month expenses chart summary by default
 myChart_title.style.display = 'none';
 myChart.style.display = 'none';
+myChart_container.style.display = 'none';
 
 myChart2_title.style.display = 'none';
 myChart2.style.display = 'none';
+myChart2_container.style.display = 'none';
 
 myChart3_title.style.display = 'none';
 myChart3.style.display = 'none';
+myChart3_container.style.display = 'none';
 
 //button to show the expense summary chart for past-one-month
 document.getElementById("pastOneMonth").addEventListener("click", function() {
   // document.getElementById("myChart").innerHTML = "Hello World";
+  myChart_container.style.display = 'block';
   myChart_title.style.display = 'block';
   myChart.style.display = 'block'; //past 1 month expense chart summary
 
+  myChart2_container.style.display = 'none';
   myChart2_title.style.display = 'none';
   myChart2.style.display = 'none'; //past 6 month expense chart summary
 
+  myChart3_container.style.display = 'none';
   myChart3_title.style.display = 'none';
   myChart3.style.display = 'none'; //past 12 month expense chart summary
 });
@@ -36,12 +45,15 @@ document.getElementById("pastOneMonth").addEventListener("click", function() {
 //button to show the expense summary chart for past-six-months
 document.getElementById("pastSixMonths").addEventListener("click", function() {
   // document.getElementById("myChart").innerHTML = "Hello World";
+  myChart_container.style.display = 'none';
   myChart_title.style.display = 'none';
   myChart.style.display = 'none';   //past 1 month expense chart summary
 
+  myChart2_container.style.display = 'block';
   myChart2_title.style.display = 'block';
   myChart2.style.display = 'block'; //past 6 month expense chart summary
 
+  myChart3_container.style.display = 'none';
   myChart3_title.style.display = 'none';
   myChart3.style.display = 'none';  //past 12 month expense chart summary
 });
@@ -49,12 +61,15 @@ document.getElementById("pastSixMonths").addEventListener("click", function() {
 //button to show the expense summary chart for past-twelve-months
 document.getElementById("pastTwelveMonths").addEventListener("click", function() {
   // document.getElementById("myChart").innerHTML = "Hello World";
+  myChart_container.style.display = 'none';
   myChart_title.style.display = 'none';
   myChart.style.display = 'none'; //past 1 month expense chart summary
 
+  myChart2_container.style.display = 'none';
   myChart2_title.style.display = 'none';
   myChart2.style.display = 'none'; //past 6 month expense chart summary
 
+  myChart3_container.style.display = 'block';
   myChart3_title.style.display = 'block';
   myChart3.style.display = 'block'; //past 12 month expense chart summary
 });
